05efa8bd820d90068c43de2f39df9da51e3bcc40
Joó Norbert <joonorbi12@gmail.com>
2020-08-29 12:06:28 +0200

    Added routing to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {AppBody} from "./App-styles"
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import Css_Builder from "./pages/Css_Builder"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBody>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Css_Builder}/>
+        </Switch>
+      </Router>
+    </AppBody>
   );
 }
 
